2b67485f5aafddb36a45cd459a4fb17d723ea3af
Memory Clutter <memclutter@gmail.com>
2015-12-11 16:45:16 +0300

    Create circliful widget

---
+++ src/Circliful.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace starcode\yii\circliful;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+
+class Circliful extends Widget
+{
+    public $tag = 'div';
+
+    public $clientOptions = [];
+
+    public $options = [];
+
+    public function init()
+    {
+        parent::init();
+        $this->options['id'] = $this->id;
+        $this->options['data'] = $this->clientOptions;
+    }
+
+    public function run()
+    {
+        CirclifulAsset::register($this->view);
+        $this->view->registerJs('$("' . $this->id . '").circliful();');
+        return Html::tag($this->tag, '', $this->options);
+    }
+}
